a14ea3f95ce98790327b4a277fd5fbf8db9df36b
Damien Leroy <dleroy@smad.be>
2026-07-20 22:35:35 +0200

    initial content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://www.be-oi.be',
+
   i18n: {
     defaultLocale: 'fr',
-    locales: ['fr', 'nl'],
+    locales: ['fr', 'nl', 'en'],
     routing: {
       prefixDefaultLocale: true,
     },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
